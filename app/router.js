5d58ed47940f370d7a33b378b822af9e91ed0860
James Byrne <jamesdesbyrne@gmail.com>
2016-07-16 13:09:39 +0100

    Adding contact about and shop routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('shop');
+  this.route('checkout');
 });
 
 export default Router;
