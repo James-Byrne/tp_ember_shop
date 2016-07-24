02f6b908446d68170b4574bad4b4042577c24b09
James Byrne <jamesdesbyrne@gmail.com>
2016-07-24 15:45:01 +0100

    Using a JSON array for products and adding addToCart for prod
    Using a JSON array for each of the products rather than a numbered array
    so that we can store properties
    
    Also added the addToCart function on the product-item component

--- app/components/product-item.js
+++ app/components/product-item.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  cart: Ember.inject.service('shopping-cart'),
+
+  actions: {
+    addToCart (product) {
+      this.get('cart').add(product);
+    }
+  }
 });
--- app/controllers/shop.js
+++ app/controllers/shop.js
@@ -2,6 +2,33 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   products: Ember.computed('property', function() {
-    return [1,2,3,4,5,6,7,8];
+    return [{
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }, {
+      name: 'test',
+      price: 100.00
+    }];
   }),
 });
