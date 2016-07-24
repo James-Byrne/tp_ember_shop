c772fb592b07b7eac8d3b0d0f7a93a2cdb5437a8
James Byrne <jamesdesbyrne@gmail.com>
2016-07-24 18:20:39 +0100

    Chaning the properties of the products

--- app/controllers/shop.js
+++ app/controllers/shop.js
@@ -1,35 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  cartItems: Ember.computed('property', function() {
-    return this.get('store').peekAll('product');
-  }),
   products: [{
-      name: 'test',
+      name: 'Product 1',
       price: 100.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 2',
+      price: 120.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 3',
+      price: 55.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 4',
+      price: 68.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 5',
+      price: 40.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 6',
+      price: 200.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 7',
+      price: 35.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 8',
+      price: 10.00
     }, {
-      name: 'test',
-      price: 100.00
+      name: 'Product 9',
+      price: 5.00
     }]
 });
