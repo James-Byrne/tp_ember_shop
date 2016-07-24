8e883431d760be979cae6449ed3dfb0c9517b27e
James Byrne <jamesdesbyrne@gmail.com>
2016-07-24 16:07:03 +0100

    Adding the show-cart component and inheriting cart from the controller

--- app/components/product-item.js
+++ app/components/product-item.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  cart: Ember.inject.service('shopping-cart'),
-
   actions: {
     addToCart (product) {
       this.get('cart').add(product);
--- app/controllers/shop.js
+++ app/controllers/shop.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  cart: Ember.inject.service('shopping-cart'),
   products: Ember.computed('property', function() {
     return [{
       name: 'test',
