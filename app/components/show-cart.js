421594f9f77c966df7dd8331179e4f8049617ba4
James Byrne <jamesdesbyrne@gmail.com>
2016-07-24 19:19:01 +0100

    Changing the layout of the shop, adding a table in th ecar tand computing the total

--- app/components/show-cart.js
+++ app/components/show-cart.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
-  cartItems: Ember.computed('store.@each.product', function() {
+  cartItems: Ember.computed('store.@each', function() {
     return this.get('store').peekAll('product');
+  }),
+  total: Ember.computed('cartItems.@each', function() {
+    let total = 0;
+    this.get('cartItems').forEach((obj) => {
+      total += parseInt(obj.get('price'));
+    });
+    return total;
   }),
 });
