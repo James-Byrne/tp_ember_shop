95cdc93ec5d73bf416606b9355055432e9489cf7
James Byrne <jamesdesbyrne@gmail.com>
2016-07-25 23:53:02 +0100

    Cart is now fully functional
    Cart functions
    ---
    The main features and functions of the cart
    
    - Add items
    - Remove items from cart
    - Display a total price
    - Show the amount of a single item in the cart
    - Correctly displaying columns in the table
    
    Other changes
    ---
    Styling changes adding mariner blue as a primary color. This still needs
    to have its colors put into variables still

--- app/components/show-cart.js
+++ app/components/show-cart.js
@@ -2,9 +2,41 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   store: Ember.inject.service(),
+
   cartItems: Ember.computed('store.@each', function() {
     return this.get('store').peekAll('product');
   }),
+
+  cartSortedItems: Ember.computed('cartItems.@each', function() {
+    const items = [];
+
+    // Get each each unique product
+    this.get('cartItems').mapBy('name').uniq().forEach(function(item) {
+      items[item] = [];
+    });
+
+    // Assign values to each product
+    this.get('cartItems').forEach(function(item) {
+      items[item.get('name')].price = item.get('price');
+
+      /**
+       * Everytime removeItem is called it removes the item
+       * with the id specified below. This value is then re-computed and
+       * another id is assigned until all records with that product name
+       * are deleted
+       */
+      items[item.get('name')].id = item.get('id');
+      
+      if (items[item.get('name')].amount) {
+        items[item.get('name')].amount += 1;
+      } else {
+        items[item.get('name')].amount = 1;
+      }
+    });
+
+    return items;
+  }),
+
   total: Ember.computed('cartItems.@each', function() {
     let total = 0;
     this.get('cartItems').forEach((obj) => {
@@ -15,9 +47,8 @@ export default Ember.Component.extend({
 
   actions: {
     removeItem(id) {
-      this.get('store').findRecord('product', id).then(function(product) {
-        product.deleteRecord();
-      });
+      const product = this.get('store').peekRecord('product', id);
+      product.deleteRecord();
     }
   }
 });
