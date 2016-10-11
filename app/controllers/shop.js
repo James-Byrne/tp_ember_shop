56cd6b5733602f5ec6531c1b6a9a2ab1b9300f0d
James Byrne <jamesdesbyrne@gmail.com>
2016-10-11 09:51:28 +0100

    Re-routing the user to the shop from index

--- app/controllers/shop.js
+++ app/controllers/shop.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // Preset values for the user
+  name: "Jamie Jones",
+  number: "4111 1111 1111 1111",
+  month: "10",
+  year: "21",
+  cvc: "222",
+  total: "10.00",
+  currency: "EUR",
+
   products: [{
       name: 'Product 1',
       price: 100.00
@@ -10,23 +19,30 @@ export default Ember.Controller.extend({
     }, {
       name: 'Product 3',
       price: 55.00
-    }, {
-      name: 'Product 4',
-      price: 68.00
-    }, {
-      name: 'Product 5',
-      price: 40.00
-    }, {
-      name: 'Product 6',
-      price: 200.00
-    }, {
-      name: 'Product 7',
-      price: 35.00
-    }, {
-      name: 'Product 8',
-      price: 10.00
-    }, {
-      name: 'Product 9',
-      price: 5.00
-    }]
+    }],
+
+  actions: {
+    submitPayment: function() {
+      Ember.$.ajax({
+        url: "http://demo-shop-realex.testingpays.com/api/pay",
+        type: "POST",
+        data: {
+          firstName: this.get('name'),
+          lastName: this.get('name'),
+          cardNumber: this.get('number'),
+          expiryMonth: this.get('month'),
+          expiryYear: this.get('year'),
+          cvv: this.get('cvc'),
+          api: this.get('api'),
+          total: this.get('total'),
+          currency: this.get('currencySelection')
+        }
+      }).done(function(res) {
+        // Create the new response item
+        // createResponse(res);
+      }).fail(function(err) {
+        // createFunctionalResponse('timeout');
+      });
+    }
+  }
 });
