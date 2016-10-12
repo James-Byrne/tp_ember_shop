99ed9db3d5d054525f60bcfb6c9adb13bc043d9f
James Byrne <jamesdesbyrne@gmail.com>
2016-10-12 17:41:44 +0100

    Implementing the remove response function and removing the kittens

--- app/components/functional-response.js
+++ app/components/functional-response.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    remove_response: function(response) {
+      this.get('responses').removeObject(response);
+    }
+  }
 });
