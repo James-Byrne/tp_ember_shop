c6d68c03941419c3f40aaf0087bc88fe74df30da
James Byrne <jamesdesbyrne@gmail.com>
2016-10-13 15:30:23 +0100

    Reworking the response codes to store the functions and thier vars as an array

--- app/services/response-codes.js
+++ app/services/response-codes.js
@@ -8,8 +8,8 @@ export default Ember.Service.extend({
     'description': 'Successful – the transaction has processed and you may proceed with the sale.',
     'checkoutmessage': 'Success. Your purchase is complete.',
     'checkoutOptions': {
-      'Ok': "rest()",
-      'Keep Shopping': "reset()"
+      'Ok': ["rest"],
+      'Keep Shopping': ["reset"]
     }
   }, {
     'code': '100',
@@ -17,8 +17,8 @@ export default Ember.Service.extend({
     'description': 'Successful – 3D secure verification needed.',
     'checkoutmessage': 'Pending - your bank requests you verify this purchase. Click Verify to open the bank verification page.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Verify': "threeDSecure()"
+      'Cancel': ["rest"],
+      'Verify': ["threeDSecure"]
     }
   }, {
     'code': '101',
@@ -26,8 +26,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: Declined by Bank – generally insufficient funds or incorrect expiry date.',
     'checkoutmessage': 'We\'re sorry but your bank has declined your transaction. Please confirm your card CVV and expiry date, then try again.',
     'checkoutOptions': {
-      'Cancel': '"rest()"',
-      'Review Detail': "highlight([cvv, expiryMonth, expiryYear])"
+      'Cancel': ["rest"],
+      'Review Detail': ["highlight", "cvv,expiry"]
     }
   }, {
     'code': '102',
@@ -35,8 +35,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: Transaction Declined Pending Offline Authorisation..',
     'checkoutmessage': 'We\'re sorry but your bank has declined your transaction. Contact your bank at +3538787878 to find out why.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Phone Bank': "rest()"
+      'Cancel': ["rest"],
+      'Phone Bank': ["rest"]
     }
   }, {
     'code': '103',
@@ -44,8 +44,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: Card reported lost or stolen.',
     'checkoutmessage': 'We\'re sorry but it looks like you\'re using a stolen card. We\'ve suspended your account pending further details.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '107',
@@ -53,8 +53,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: Your fraud checks blocked the transaction.',
     'checkoutmessage': 'We\'re sorry, you seem to be a fraudster. Contact us if you wish to dispute this.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '600',
@@ -62,8 +62,8 @@ export default Ember.Service.extend({
     'description': 'Generic error 600',
     'checkoutmessage': 'Empty message',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '601',
@@ -71,8 +71,8 @@ export default Ember.Service.extend({
     'description': 'Generic error 601',
     'checkoutmessage': '',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '603',
@@ -80,8 +80,8 @@ export default Ember.Service.extend({
     'description': 'Generic error 603',
     'checkoutmessage': '',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '666',
@@ -89,8 +89,8 @@ export default Ember.Service.extend({
     'description': 'Account deactivated',
     'checkoutmessage': '',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '200',
@@ -98,8 +98,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: bank communication error.',
     'checkoutmessage': '',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '205',
@@ -107,8 +107,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: bank communication error.',
     'checkoutmessage': '',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Contact Us': "contactUs()"
+      'Cancel':["rest"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '501',
@@ -116,9 +116,9 @@ export default Ember.Service.extend({
     'description': 'This transaction has already been processed.',
     'checkoutmessage': 'We\'re sorry, a technical issue has stopped your purchase from completing. Your card has not been charged. Re-try to complete the purchase or contact us to talk more about this problem.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Retry': "retry()",
-      'Contact Us': "contactUs()"
+      'Cancel': ["rest"],
+      'Retry': ["retry"],
+      'Contact Us': ["contactUs"]
     }
   }, {
     'code': '507',
@@ -126,11 +126,11 @@ export default Ember.Service.extend({
     'description': 'Currency/card combination not allowed.',
     'checkoutmessage': 'We\'re sorry, that item is not available to buy in that currency. You can complete your purchase using one of our alternatives.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'EUR': "currency(EUR)",
-      'GBP': "currency(GBP)",
-      'USD': "currency(USD)",
-      'RUB': "currency(RUB)"
+      'Cancel': ["rest"],
+      'EUR': ['change_currency', 'EUR'],
+      'GBP': ['change_currency', 'GBP'],
+      'USD': ['change_currency', 'USD'],
+      'RUB': ['change_currency', 'RUB']
     }
   }, {
     'code': '509',
@@ -138,8 +138,8 @@ export default Ember.Service.extend({
     'description': 'Card expiry date is invalid',
     'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the card expiry date on your card and retry.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Verify Expiry': "highlight([expiryMonth, expiryYear])"
+      'Cancel': ["rest"],
+      'Verify Expiry': ["highlight", "expiry"]
     }
   }, {
     'code': '502',
@@ -147,8 +147,8 @@ export default Ember.Service.extend({
     'description': 'Invalid cardholder name',
     'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the cardholder name and retry.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Verify Name': "highlight([firstName, lastName])"
+      'Cancel': ["rest"],
+      'Verify Name': ["highlight", "firstName,lastName"]
     }
   }, {
     'code': '101N',
@@ -156,8 +156,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: CVV Not Matched.',
     'checkoutmessage': 'We\'re sorry, we could not process your card request. Please verify the card CVV and retry.',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Re-enter CVV': "highlight([cvv])"
+      'Cancel': ["rest"],
+      'Re-enter CVV': ["highlight", "cvv"]
     }
   }, {
     'code': '101I',
@@ -165,8 +165,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: CVV Not checked due to circumstances.',
     'checkoutmessage': 'Warning, there was a problem verifying your CVV. Do you wish to complete the purchase without verifying CVV?',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Re-enter CVV': "highlight([cvv])"
+      'Cancel': ["rest"],
+      'Re-enter CVV': ["highlight", "cvv"]
     }
   }, {
     'code': '101U',
@@ -174,8 +174,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: CVV Issuer not certified.',
     'checkoutmessage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
     'checkoutOptions': {
-      'Cancel': "reset()",
-      'Try with Paypal': "otherProcessor()"
+      'Cancel': ["reset"],
+      'Try with Paypal': ["otherProcessor"]
     }
   }, {
     'code': '101P',
@@ -183,8 +183,8 @@ export default Ember.Service.extend({
     'description': 'A failed transaction specifically meaning: CVV Not processed.',
     'checkoutmessage': 'We\'re sorry, we could not process your card request. Your card issuer does not support CVV codes - we cannot accept such cards.',
     'checkoutOptions': {
-      'Cancel': "reset()",
-      'Try with Paypal': "otherProcessor()"
+      'Cancel': ["reset"],
+      'Try with Paypal': ["otherProcessor"]
     }
   }, {
     'code': 'timeout',
@@ -192,16 +192,14 @@ export default Ember.Service.extend({
     'description': 'Timeout.',
     'checkoutmessage': 'Sorry, we experienced a problem sending your request to your bank and your transaction was not successfull. ',
     'checkoutOptions': {
-      'Cancel': "rest()",
-      'Retry': "retry()"
+      'Cancel': ["rest"],
+      'Retry': ["retry"]
     }
-  });
+  }],
 
-  find_response(element, index, array) {
-    return resultCode === element.code;
-  }
-
-  get_response_code(code) {
-    return this.get('responses').find(this.find_response)
+  get_response_code(resultCode) {
+    return this.get('responses').find((element) => {
+      return resultCode === element.code;
+    });
   }
 });
